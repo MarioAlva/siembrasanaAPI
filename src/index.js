3be54d4b40ecebb4299153d5968c5cd0c7a5fff6
MarioAlva <marioantonioalva@gmail.com>
2023-11-16 16:22:19 +0100

    Service restored

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const { sequelize } = require('./models');
 const productRouter = require('./routes/product');
+const cors = require('cors');
+
 const app = express()
 const port = process.env.PORT ?? 3000;
+
+app.use(cors({
+	  origin: (origin, allowFn) => {
+		    allowFn(null, 'http://localhost:3000');
+		    allowFn(null, 'https://siembrasana.bio');
+		},
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
